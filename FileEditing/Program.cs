afdc7fc5f3b07236254619c35c540969a445eb1a
Zoltan Lengyel <zoltanlengyel77@gmail.com>
2018-10-30 16:03:58 +0100

    Read File by bytes

--- FileEditing/Program.cs
+++ FileEditing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,25 @@ namespace FileEditing
     {
         static void Main(string[] args)
         {
+            string localFile = "../../files/test.txt";
+            if (File.Exists(localFile))
+            {
+                FileStream fs = new FileStream(localFile, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+                Console.WriteLine(fs.Name);
+                Console.WriteLine(fs.Length);
+                Console.WriteLine(fs.Position);
+                while (fs.Position < fs.Length)
+                {
+                    int actualChar = fs.ReadByte();
+                    Console.WriteLine("The actual bíte is: " + actualChar + " in char: " + (char)(actualChar));
+                    //Console.WriteLine((char)(155));
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR 404 - A fájl nem található! [" + localFile +"]");
+            }
+            Console.ReadKey();
         }
     }
 }
