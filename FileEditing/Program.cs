234c1d2147878783cfef1508abc933de3a84eab7
Zoltan Lengyel <zoltanlengyel77@gmail.com>
2018-10-30 16:22:46 +0100

    Array contains check

--- FileEditing/Program.cs
+++ FileEditing/Program.cs
@@ -11,18 +11,22 @@ namespace FileEditing
     {
         static void Main(string[] args)
         {
-            string localFile = "../../files/test.txt";
+            //string localFile = "../../files/test.txt";
+            string localFile = "../../files/kerdesek.txt";
             if (File.Exists(localFile))
             {
                 FileStream fs = new FileStream(localFile, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
                 Console.WriteLine(fs.Name);
                 Console.WriteLine(fs.Length);
-                Console.WriteLine(fs.Position); 
+                Console.WriteLine(fs.Position);
+                int[] array = new int[] { 59, 10, 32, 13 };
                 while (fs.Position < fs.Length)
                 {
                     int actualChar = fs.ReadByte();
-                    Console.WriteLine("The actual bíte is: " + actualChar + " in char: " + (char)(actualChar));
-                    
+                    if (!array.Contains(actualChar))
+                    {
+                        Console.WriteLine("The actual bíte is: " + actualChar + " in char: " + (char)(actualChar));
+                    }
                 }
                 Console.WriteLine((char)(155));
                 Console.WriteLine((char)(055));
